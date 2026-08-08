1006f8d40326927c4fd93b3dd3b071da1d59eaf8
hamzuraz <280989739+hamzuraz@users.noreply.github.com>
2026-08-08 20:19:47 +0700

    fix: system theme detection

--- src/scripts/initTheme.ts
+++
@@ -1,6 +0,0 @@
-document.documentElement.dataset.theme =
-	localStorage.theme === "dark" ||
-	(!("theme" in localStorage) &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches)
-		? "dark"
-		: "light";
