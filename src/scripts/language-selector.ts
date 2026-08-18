da8f8744dc105611e6edb950d8b2ba21b83bb377
Hamzuraz <hamzurazio@gmail.com>
2026-08-18 15:34:04 +0700

    feat: add manual multi-language (i18n) support

---
+++ src/scripts/language-selector.ts
@@ -0,0 +1,15 @@
+import { buildLocalizedPath, getLangFromPathname } from "$/i18n/utils.ts";
+
+const languageSelector = document.getElementById("language-selector");
+
+if (languageSelector instanceof HTMLSelectElement) {
+	languageSelector.value = getLangFromPathname(window.location.pathname);
+}
+
+languageSelector?.addEventListener("change", (event: Event) => {
+	const element = event.target;
+	if (element instanceof HTMLSelectElement) {
+		const newPath = buildLocalizedPath(window.location.pathname, element.value);
+		window.location.href = newPath;
+	}
+});
