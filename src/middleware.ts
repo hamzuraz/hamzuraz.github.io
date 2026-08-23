f5180da88e0fdb3b2d3fd8e7ec5f3869e82e8004
Hamzuraz <hamzurazio@gmail.com>
2026-08-23 19:52:35 +0700

    refactor: restructure language and theme handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { defineMiddleware } from "astro:middleware";
-import { getLangFromPathname, useTranslations } from "$/i18n/utils.ts";
+import { getLangCodeFromPathname, useTranslations } from "$/i18n/utils.ts";
 
 export const onRequest = defineMiddleware((context, next) => {
-	const lang = getLangFromPathname(context.url.pathname);
+	const langCode = getLangCodeFromPathname(context.url.pathname);
 
-	context.locals.lang = lang;
-	context.locals.t = useTranslations(lang);
+	context.locals.langCode = langCode;
+	context.locals.t = useTranslations(langCode);
 
 	return next();
 });
--- src/scripts/language-selector.ts
+++ src/scripts/language-selector.ts
@@ -1,9 +1,9 @@
-import { buildLocalizedPath, getLangFromPathname } from "$/i18n/utils.ts";
+import { buildLocalizedPath, getLangCodeFromPathname } from "$/i18n/utils.ts";
 
 const languageSelector = document.getElementById("language-selector");
 
 if (languageSelector instanceof HTMLSelectElement) {
-	languageSelector.value = getLangFromPathname(window.location.pathname);
+	languageSelector.value = getLangCodeFromPathname(window.location.pathname);
 
 	languageSelector.addEventListener("change", (event: Event) => {
 		const element = event.target;
