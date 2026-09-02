cf0211f2c19c07852b4ab0e9d0210a5208019f09
Hamzuraz <hamzurazio@gmail.com>
2026-09-02 09:28:56 +0700

    refactor(404): extract language routing logic into scripts

---
+++ src/scripts/not-found/utils.ts
@@ -0,0 +1,45 @@
+import { isLangCode, type LangCode } from "../theme-selector/type-helpers";
+
+type LangCodeRecord = Record<LangCode, string>;
+export const langCodeSegments: LangCodeRecord = {
+	"en-US": "",
+	id: "id",
+	ja: "ja",
+};
+
+export const languages: LangCodeRecord = {
+	"en-US": "English",
+	id: "Bahasa Indonesia",
+	ja: "日本語",
+};
+
+export function getObjectKeys<T extends object>(obj: T): (keyof T)[] {
+	return Object.keys(obj) as (keyof T)[];
+}
+
+export function buildLocalizedRoutePath(
+	langCode: string,
+	pathname: string,
+): string {
+	if (!isLangCode(langCode)) return pathname;
+
+	const langCodeSegment = langCodeSegments[langCode];
+	const segments = pathname.split("/").filter(Boolean);
+
+	const hasTrailingSlash = pathname.endsWith("/");
+	const hasLangCodeSegment = getObjectKeys(langCodeSegments).some(
+		(code) => code === segments[0],
+	);
+
+	if (hasLangCodeSegment) segments.shift();
+
+	const localizedRoutePath = langCodeSegment
+		? `/${langCodeSegment}/${segments.join("/")}`
+		: `/${segments.join("/")}`;
+
+	if (hasTrailingSlash && !localizedRoutePath.endsWith("/")) {
+		return `${localizedRoutePath}/`;
+	}
+
+	return localizedRoutePath;
+}
